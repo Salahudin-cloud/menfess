70602205be2c02f8640fcc6e1b191d83a8f8587e
Quezev <dimasagung1734@gmail.com>
2023-12-05 01:54:11 +0700

    memperbarui navbarnya

--- app/Views/imports/templates/frontend/navbar.php
+++ app/Views/imports/templates/frontend/navbar.php
@@ -13,14 +13,14 @@
 			<div class="site-navigation">
 			  <div class="row g-0 align-items-center">
 				<div class="col-1">
-				  <a href="<?php echo site_url('/')?>" class="logo m-0 float-start"
+				  <a href="<?php echo site_url('/halaman_utama')?>" class="logo m-0 float-start"
 					>UnveilU<span class="text-primary">.</span></a>
 				</div>
 				<div class="col-10 text-end">
 				  <ul
 					class="js-clone-nav d-none d-lg-inline-block text-start site-menu mx-auto"
 				  >
-					<li><a href="<?php echo site_url('/')?>">Home</a></li>
+					<li><a href="<?php echo site_url('/halaman_utama')?>">Home</a></li>
 					<li><a href="<?php echo site_url('/menfess')?>">Menfess</a></li>
 					<li><a href="<?php echo site_url('/confess')?>">Confess</a></li>
 					<li><a href="<?php echo site_url('/artikel')?>">Artikel</a></li>
